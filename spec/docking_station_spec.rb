23ca493a0c2c20d2deb5c61c2984ee7c42d0d0a6
David Halewood <davidnhalewood@hotmail.com>
2017-09-27 12:48:01 +0100

    adding release_bike test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
-
+    it "should release bike from docking station" do
+      expect(subject).to respond_to :release_bike
+       # subject is an instantiated DockingStation class object
+    end
 end
+
+# implicitly defined version
+# it { is_expected.to respond_to :release_bike }
