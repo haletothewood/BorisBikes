36936c05701cf966f3edb3e6b85ae649f6408427
David Halewood <davidnhalewood@hotmail.com>
2017-09-27 17:33:08 +0100

    refactored a little

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,16 +4,9 @@ class DockingStation
 
 attr_reader :bike
 
-  def initialize(amount_of_bikes = 0)
-    @amount_of_bikes = amount_of_bikes
-  end
-
   def release_bike
-    if @amount_of_bikes > 0
-    # create a new bike object
-      Bike.new
-    end
-    raise "Sorry, no bikes!"
+    fail "Sorry, no bikes!" unless @bike
+    @bike
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,14 +7,16 @@ describe DockingStation do
        # implicitly defined version
        # it { is_expected.to respond_to :release_bike }
     end
-    it "should create a new bike object" do
-     expect {subject.release_bike}.to raise_error "Sorry, no bikes!"
-    end
     it "should release a working bike" do
       expect(Bike.new.working?).to eq true
     end
     it "should dock a bike" do
       bike = Bike.new # setting up the bike object for the test
       expect(subject.dock(bike)).to eq bike
+    end
+    describe '#release_bike' do
+      it "should create a new bike object" do
+      expect {subject.release_bike}.to raise_error "Sorry, no bikes!"
+      end
     end
 end
