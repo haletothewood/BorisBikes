e7853354ed54987fb1d08b0cf2c2ee3debfc0588
David Halewood <davidnhalewood@hotmail.com>
2017-09-27 14:54:02 +0100

    adding working? test and method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,5 @@
 require 'bike'
 
 describe Bike do
-  it 'should respond to working?' do
-    expect(subject).to respond_to :working?
-  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,8 +4,13 @@ describe DockingStation do
     it "should release bike from docking station" do
       expect(subject).to respond_to :release_bike
        # subject is an instantiated DockingStation class object
+       # implicitly defined version
+       # it { is_expected.to respond_to :release_bike }
+    end
+    it "should create a new bike object" do
+     expect(subject.release_bike).to be_a Bike
+    end
+    it "should release a working bike" do
+      expect(Bike.new.working?).to eq true
     end
 end
-
-# implicitly defined version
-# it { is_expected.to respond_to :release_bike }
