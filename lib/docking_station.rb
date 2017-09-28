7e4910a6b30b41ad06d2e480fda971b8e76b3a4c
Jon Sanders <jonsanders101@aol.com>
2017-09-28 15:13:48 +0100

    Created capacity for dock

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,16 +2,21 @@ require './lib/bike.rb'
 
 class DockingStation
 
-attr_reader :bike
+attr_accessor :bikes, :capacity
+
+  def initialize (capacity = 20)
+    @capacity = capacity
+    @bikes = []
+  end
 
   def release_bike
-    fail "Sorry, no bikes!" unless @bike
-    @bike
+    fail "Sorry, no bikes!" if @bikes.length == 0
+    @bikes.pop
   end
 
   def dock(bike)
-    fail "Docking station full!" if @bike # as capacity is 1
-    @bike = bike
+    fail "Docking station full!" if @bikes.length == 20
+    @bikes << bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,15 @@
 require 'docking_station'
 
 describe DockingStation do
+
+    it "should have a capacity of 20 bikes" do
+      expect(subject.capacity).to eq 20
+    end
+
+    it "can store 0 to 20 bikes" do
+      expect(subject.bikes).to eq []
+    end
+
     it "should release bike from docking station" do
       expect(subject).to respond_to :release_bike
        # subject is an instantiated DockingStation class object
@@ -10,16 +19,23 @@ describe DockingStation do
     it "should release a working bike" do
       expect(Bike.new.working?).to eq true
     end
+
     describe '#dock' do
       it "raises and error when full" do
-        # as capacity is 1 we need to instantiate a bike first before running test
-        subject.dock(Bike.new)
-        expect { subject.dock(Bike.new) }.to raise_error "Docking station full!"
+        20.times {subject.dock(Bike.new)}
+        expect { subject.dock(Bike.new) }.to raise_error "Docking station full!" if subject.bikes.length == 20
       end
     end
     describe '#release_bike' do
       it "raises an error when there are no bikes available" do
-      expect {subject.release_bike}.to raise_error "Sorry, no bikes!"
+      expect {subject.release_bike}.to raise_error "Sorry, no bikes!" if subject.bikes.length == 0
+      end
+
+      it "reduces the number of bikes stored by 1" do
+        subject.dock(Bike.new)
+        before = subject.bikes.length
+        subject.release_bike
+        expect(subject.bikes.length).to eq (before - 1)
       end
     end
 end
